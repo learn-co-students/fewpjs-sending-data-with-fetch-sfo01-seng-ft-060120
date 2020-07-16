293cb29f1a82501ec3e349732c403c247428a9ad
iona-b <ionabrabender@gmail.com>
2020-07-16 10:42:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData (userName, userEmail) {
+
+    let formData = {
+        name: userName,
+        email: userEmail
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"];
+        })
+        .catch(function(error){
+            alert("Uh oh! Something went wrong...");
+            console.log(error.message);
+        });
+
+}
